81456d40fd399636266047409a78d3ff70a2d84e
Oleh <klimoleh045>
2024-03-08 20:29:21 +0200

    lesson 08.03

--- db_scripts.py
+++ db_scripts.py
@@ -1,14 +1,13 @@
 import sqlite3
 
+db_name = "blog.db"
 connection = None
 cursor = None
 
-
 def open():
-    global connection, cursor
-    connection =sqlite3.connect("blog.db")
+    global connection,cursor
+    connection = sqlite3.connect(db_name)
     cursor = connection.cursor()
-
 
 def close():
     cursor.close()
@@ -20,3 +19,42 @@ def getUser():
     user = cursor.fetchone()
     close()
     return user
+
+
+def getIdByCategory(category_name):
+    open()
+    cursor.execute('''  SELECT category_id
+                        FROM category
+                        WHERE category_name = (?)
+
+                   ''', [category_name])
+    category_id = cursor.fetchone()[0]
+    close()
+    return category_id
+
+
+def getPostsByCategory(category_name):
+    open()
+    cursor.execute('''  SELECT * FROM post, category
+                        WHERE category_name = (?)
+                        AND post.category_id = category.category_id
+                        ORDER BY post_datetime DESC
+                   ''', [category_name])
+    posts = cursor.fetchall()
+    close()
+    return posts
+
+def addPost(category_id, post_text):
+    open()
+    cursor.execute('''INSERT INTO post (category_id, post_text)
+                      VALUES ((?), (?)) 
+                   ''', [category_id, post_text])
+    connection.commit()
+    close()
+
+
+def deletePost(post_id):
+    open()
+    cursor.execute('''DELETE FROM post WHERE post_id=(?) ''', [post_id])
+    connection.commit()
+    close()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 import settings as stg
 import db_scripts as db
 
@@ -11,21 +11,43 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/post/category")
-def post_category():
-    return "ТУТ БУДУТЬ КАТЕГОРІЇ ПОСТІВ" 
-
-
-@app.route("/post/view")
-def post_view():
-    return "ТУТ БУДЕ МІЙ ПОСТ" 
-
-
 @app.route("/about")
 def about():
     user = db.getUser()
     print(user)
     return render_template("about.html", user=user)
 
+
+@app.route("/categories")
+def post_categories():
+    return render_template("categories.html")
+
+
+@app.route("/post/category/<category_name>", methods=["GET", "POST"])
+def post_category(category_name):
+    category_id = db.getIdByCategory(category_name)
+
+    if request.method == "GET":
+        posts = db.getPostsByCategory(category_name)
+    
+    if  request.method == "POST":
+        db.addPost(request.form["category_id"], request.form["post"])
+        posts = db.getPostsByCategory(category_name)
+        return redirect(f"/post/category/{category_name}")
+
+    return render_template("post_category.html", 
+                           category_name=category_name,
+                           category_id=category_id,
+                           posts = posts) 
+
+@app.route("/post/view")
+def post_view():
+    return "ТУТ БУДЕ МІЙ ПОСТ" 
+
+
+@app.route("/post/delete/<post_id>/<category_name>")
+def deletePost(post_id, category_name):
+    db.deletePost(post_id)
+    return redirect(f"/post/category/{category_name}")
 
 app.run(debug=True)
